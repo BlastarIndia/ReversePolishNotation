16a60ce1f2dd013d91039f290347dc2ad7542464
Trevor Elkins <elkins.trevor@gmail.com>
2014-01-17 01:46:09 -0500

    fixed minor bug and added comments

--- src/com/trevore/rpn/RpnCalculator.java
+++ src/com/trevore/rpn/RpnCalculator.java
@@ -17,14 +17,25 @@ public class RpnCalculator {
     public RpnCalculator(boolean log) {
         mPrintLogging = log;
     }
-    
+
+    /**
+     * Turns on or off logging.  If enabled it will print each PUSH and POP to the Stack.
+     * @param enabled
+     */
     public void setLogging(boolean enabled) {
         mPrintLogging = enabled;
     }
 
+    /**
+     * Calculate the result of the RPN String.  Returns the value as a double.
+     * @param input
+     * @return
+     */
     public double calculate(String input) {
-        if(mInputQueue.size() != 0)
+        if(mInputQueue.size() != 0) {
             mInputQueue = new ArrayDeque<Token>(input.length());
+            mCurrentToken = 0;
+        }
 
         StringTokenizer tokenizer = new StringTokenizer(input, " ");
         while(tokenizer.hasMoreTokens()) {
@@ -38,36 +49,44 @@ public class RpnCalculator {
         return mInputQueue.pop().getValue();
     }
 
-    private void processToken(String input) {
+    /**
+     * Process an individual token parsed from the RPN String
+     * @param token The token being processed from the RPN String
+     */
+    private void processToken(String token) {
         //Single char, check its type for correct parsing
-        if(input.length() == 1) {
-            char temp = input.charAt(0);
+        if(token.length() == 1) {
+            char temp = token.charAt(0);
             //Digit
             if(Character.isDigit(temp)) {
-                mInputQueue.push(new ValueToken(input));
-                if(mPrintLogging) System.out.println("Pushing input " + input);
+                mInputQueue.push(new ValueToken(token));
+                if(mPrintLogging) System.out.println("Pushing token " + token);
             }
             //Math symbol such as + - * /
             else if(isMathSymbol(temp)) {
-                processOperator(input);
+                processOperator(token);
             }
             //Illegal character
             else {
-                throw new IllegalArgumentException("Invalid input character " + input + " at position " + mCurrentToken + ".");
+                throw new IllegalArgumentException("Invalid token character " + token + " at position " + mCurrentToken + ".");
             }
         }
         //Has to be a digit if length > 1
         else {
-            mInputQueue.add(new ValueToken(input));
-            if(mPrintLogging) System.out.println("Pushing input " + input);
+            mInputQueue.add(new ValueToken(token));
+            if(mPrintLogging) System.out.println("Pushing token " + token);
         }
     }
 
-    private void processOperator(String input) {
+    /**
+     * An operator was detected as the current token and needs to be processed
+     * @param operand The operand detected
+     */
+    private void processOperator(String operand) {
         if(mInputQueue.size() < 2)
-            throw new IllegalArgumentException("Does not have two operands for operation " + input + " at position " + mCurrentToken + ".");
+            throw new IllegalArgumentException("Does not have two operands for operation " + operand + " at position " + mCurrentToken + ".");
 
-        char operator = input.charAt(0); //this is safe becauase only called if length is 1
+        char operator = operand.charAt(0); //this is safe becauase only called if length is 1
         double first = mInputQueue.pop().getValue();
         double second = mInputQueue.pop().getValue();
         ValueToken result = null;
@@ -96,8 +115,13 @@ public class RpnCalculator {
         if(result != null && mPrintLogging) System.out.println("Pushing result " + result.getValue());
     }
 
-    private boolean isMathSymbol(char input) {
-        switch(input) {
+    /**
+     * Determines if a token is a math symbol (+,-,/,*) or not
+     * @param token The token being examined
+     * @return True if a math symbol (+,-,/,*), false otherwise
+     */
+    private boolean isMathSymbol(char token) {
+        switch(token) {
             case '+':
             case '-':
             case '/':
